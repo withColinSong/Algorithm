648fb24df9d9e0a6bf45fa6c5d28f8cf6799edb5
withColinSong <withcolinsong@gmail.com>
2021-04-29 22:58:56 +0900

    add: 숫자만 추출

---
+++ Inflearn/1/01/1-09.java
@@ -0,0 +1,24 @@
+public class Main {
+	public int solution(String str) {
+		String answer = "";
+		
+		for(int i=0; i<str.length(); i++) {
+			char c = str.charAt(i);
+			if(47 < c && c < 57) {
+				answer += c;
+			}
+		}
+		
+		int out = Integer.parseInt(answer);
+		return out;
+	}
+	
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		System.out.println(main.solution(input));
+
+	}
+}
