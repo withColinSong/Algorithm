9d05f1963b472a8a47145f17dc6033e259de3265
withColinSong <withcolinsong@gmail.com>
2021-05-20 21:36:43 +0900

    fix: 뒤집은 소수

--- Inflearn/1/02/2-06.java
+++ Inflearn/1/02/2-06.java
@@ -1,37 +1,45 @@
+package algorthm;
+
 import java.util.*;
-class Main {	
-	public boolean isPrime(int num){
-		if(num==1) return false;
-		for(int i=2; i<num; i++){
-			if(num%i==0) return false;
+
+public class Main {
+	public boolean isPrime(int num) {
+		if(num == 1) return false;
+		for(int i=2; i<num; i++) {
+			if(num % i == 0) return false; 
 		}
 		return true;
 	}
-
-	public ArrayList<Integer> solution(int n, int[] arr){
+	public ArrayList<Integer> solution(int n, int[] arr) {
+		
 		ArrayList<Integer> answer = new ArrayList<>();
-		for(int i=0; i<n; i++){
-			int tmp=arr[i];
-			int res=0;
-			while(tmp>0){
-				int t=tmp%10;
-				res=res*10+t;
-				tmp=tmp/10;
+		for(int i=0; i<n; i++) {
+			int tmp = arr[i];
+			int res = 0;
+			while(tmp > 0) {
+				int t = tmp % 10;
+				res = res * 10 + t;
+				tmp = tmp / 10;
 			}
+			
 			if(isPrime(res)) answer.add(res);
 		}
+
 		return answer;
 	}
-	public static void main(String[] args){
-		Main T = new Main();
+	
+	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		int n=scan.nextInt();
-		int[] arr=new int[n];
-		for(int i=0; i<n; i++){
-			arr[i]=scan.nextInt();
+		Main main = new Main();
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
 		}
-		for(int x : T.solution(n, arr)){
-			System.out.print(x+" ");
+		
+		for(int x: main.solution(n, arr)) {
+			System.out.println(x+ " ");
 		}
 	}
 }
